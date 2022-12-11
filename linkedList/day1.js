33019470a17233ef99f023d84dc03b6f8b9536cb
dev-ahmedali <ahmedali69325@gmail.com>
2022-12-11 15:25:26 +0600

    added linked list day 2

--- linkedList/day1.js
+++ linkedList/day1.js
@@ -10,16 +10,67 @@ class LinkedList {
     let node = new Node(value);
     this.head = node;
     this.tail = node;
+    this.length = 1;
   }
   appned(value) {
-   let node = new Node(value)
-   this.tail.next = node
-   this.tail = node
+    let node = new Node(value);
+    this.tail.next = node;
+    this.tail = node;
+    this.length++;
+  }
+
+  prepend(value) {
+    let node = new Node(value, this.head);
+    // node.next = this.head
+    this.head = node;
+    this.length++;
+  }
+
+  print() {
+    let data = this.head;
+    while (data != null) {
+      console.log(data.value);
+      data = data.next;
+    }
+  }
+
+  appendAt(value, position) {
+    if (position === 1) {
+      this.prepend(value);
+    } else if (position === this.length + 1) {
+      this.appned(value);
+    } else {
+      let node = new Node(value);
+      let prevNode = this.findNode(position - 1);
+      node.next = prevNode.next;
+      prevNode.next = node;
+      this.length++
+      console.log({ prevNode: prevNode.value });
+    }
+  }
+
+  findNode(n) {
+    let count = 1;
+    let data = this.head;
+    while (data) {
+      if (count == n) break;
+      count++;
+      data = data.next;
+    }
+    return data;
   }
 }
 
 let list = new LinkedList(100);
-list.appned(200)
-list.appned(300)
-list.appned(400)
+list.appned(200);
+list.appned(300);
+list.appned(400);
+list.prepend(50);
+
+list.appendAt(-5, 1);
+list.appendAt(1000, 7);
+list.appendAt(70, 3);
+
+list.print();
+// console.log(list.head);
 console.log(list);
