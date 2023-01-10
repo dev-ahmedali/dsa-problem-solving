c355f5ec8ef94fe081728b37d24529dbf731ed54
dev-ahmedali <ahmedali69325@gmail.com>
2023-01-11 03:49:44 +0600

    solved valid perfact square

---
+++ searching/367.js
@@ -0,0 +1,22 @@
+var isPerfectSquare = function (num) {
+  if (num < 1) {
+    return false;
+  }
+  if (num == 1) {
+    return true;
+  }
+  let left = 1;
+  let right = num;
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+    if (mid * mid === num) {
+      return true;
+    } else if (mid * mid > num) {
+      right = mid - 1;
+    } else if (mid * mid < num) {
+      left = mid + 1;
+    }
+  }
+  return false;
+};
