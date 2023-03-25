f8ab151014be6fcb797e9d38121fe848e8f47c0f
dev-ahmedali <ahmedali69325@gmail.com>
2023-03-25 23:21:21 +0600

    update

--- sorting/quickSort.js
+++ sorting/quickSort.js
@@ -1,23 +1,21 @@
-
-
-function partition(arr, l, r){
-    let pivot = arr[l];
-    let count = 0;
-    for(let i=l; i<= r; i++){
-        if(arr[i] < pivot){
-            count++;
-        }
+function partition(arr, l, r) {
+  let pivot = arr[l];
+  let count = 0;
+  for (let i = l; i <= r; i++) {
+    if (arr[i] < pivot) {
+      count++;
     }
+  }
 }
 
-function quickSort(arr, l, r){
-    if(l<r){
-        let p = partition(arr, l, r);
+function quickSort(arr, l, r) {
+  if (l < r) {
+    let p = partition(arr, l, r);
 
-        // quickSort(arr, l, p-1)
-        // quickSort(arr, p+1, r)
-    }
+    // quickSort(arr, l, p-1)
+    // quickSort(arr, p+1, r)
+  }
 }
 
 let arr = [8, 7, 6, 1, 0, 9, 2];
-quickSort(arr, 0, arr.length-1)
+quickSort(arr, 0, arr.length - 1);
