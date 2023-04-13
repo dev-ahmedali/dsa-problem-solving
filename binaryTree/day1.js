d9576581d5416be99a031ebf4e6f62a69355893b
dev-ahmedali <ahmedali69325@gmail.com>
2023-04-13 14:26:00 +0600

    binary tree 1

---
+++ binaryTree/day1.js
@@ -0,0 +1,27 @@
+
+class Node{
+    constructor(value, left = null, right = null) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+class binaryTree {
+    constructor(value){
+        this.root = new Node(value)
+    }
+
+    insert(value) {
+        let current = this.root
+    }
+}
+
+let tree = new binaryTree(10);
+
+tree.insert(20)
+
+
+
+
