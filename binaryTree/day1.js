14de9499d16041881cb102f38b7e7f43c252ff39
dev-ahmedali <ahmedali69325@gmail.com>
2023-04-14 02:11:31 +0600

    added binary tree day1

--- binaryTree/day1.js
+++ binaryTree/day1.js
@@ -1,27 +1,41 @@
-
-class Node{
-    constructor(value, left = null, right = null) {
-        this.value = value;
-        this.left = left;
-        this.right = right;
-    }
+class Node {
+  constructor(value, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
 }
 
-
 class binaryTree {
-    constructor(value){
-        this.root = new Node(value)
-    }
+  constructor(value) {
+    this.root = new Node(value);
+  }
 
-    insert(value) {
-        let current = this.root
+  insert(value) {
+    let current = this.root;
+    while (true) {
+      if (current.value < value) {
+        if (!current.right) {
+          let newNode = new Node(value);
+          current.right = newNode;
+          break;
+        }
+        current = current.right
     }
+        else {
+            if(!current.left) {
+                let newNode = new Node(value)
+                current.left = newNode;
+                break;
+            }
+            current = current.left
+        }
+       
+  
+    }
+  }
 }
 
 let tree = new binaryTree(10);
 
-tree.insert(20)
-
-
-
-
+tree.insert(20);
