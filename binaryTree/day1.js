a995ab814e01404e7a7e0c9bbca9cb631b8ac682
dev-ahmedali <ahmedali69325@gmail.com>
2023-04-25 14:57:55 +0600

    added bst

--- binaryTree/day1.js
+++ binaryTree/day1.js
@@ -20,22 +20,41 @@ class binaryTree {
           current.right = newNode;
           break;
         }
-        current = current.right
-    }
-        else {
-            if(!current.left) {
-                let newNode = new Node(value)
-                current.left = newNode;
-                break;
-            }
-            current = current.left
+        current = current.right;
+      } else {
+        if (!current.left) {
+          let newNode = new Node(value);
+          current.left = newNode;
+          break;
         }
-       
-  
+        current = current.left;
+      }
     }
+  }
+
+  search(value) {
+    let current = this.root;
+    while (current) {
+      if (current.value === value) {
+        return "Data found";
+      } else if (current.value > value) {
+        // go to left
+        current = current.left;
+      } else {
+        // go to right
+        current = current.right;
+      }
+    }
+    return "Not found"
   }
 }
 
 let tree = new binaryTree(10);
 
 tree.insert(20);
+tree.insert(9);
+tree.insert(8);
+tree.insert(90);
+console.log(tree.search(10))
+
+console.log(tree.root);
