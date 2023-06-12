b41cfaed5390768b5f51d9edcfe509ae3f0e739b
dev-ahmedali <ahmedali69325@gmail.com>
2023-06-13 02:25:19 +0600

    added containDuplicate

---
+++ hash/containsDuplicate.js
@@ -0,0 +1,9 @@
+var containsDuplicate = function (nums) {
+  nums = nums.sort();
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] == nums[i + 1]) {
+      return true;
+    }
+  }
+  return false;
+};
